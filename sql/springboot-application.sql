91626194badfd4283716a6a699ce98a06632ca95
caoxm <caoxm@citydo.com.cn>
2018-11-22 15:25:48 +0800

    sql脚本

---
+++ sql/springboot-application.sql
@@ -0,0 +1,51 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 【mysql】localhost【本地】
+ Source Server Type    : MySQL
+ Source Server Version : 50724
+ Source Host           : localhost:3306
+ Source Schema         : springboot-application
+
+ Target Server Type    : MySQL
+ Target Server Version : 50724
+ File Encoding         : 65001
+
+ Date: 22/11/2018 15:25:11
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tb_phone
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_phone`;
+CREATE TABLE `tb_phone`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '手机号码',
+  `code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '验证码',
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  `update_time` datetime(0) NULL DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT 0 COMMENT '0:未验证 1:已验证',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `TB_PHONE_PHONE`(`phone`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for tb_user
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_user`;
+CREATE TABLE `tb_user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  `update_time` datetime(0) NULL DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT 0 COMMENT '0:可用 1:删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
